813b3625dbc1c4d24ea035cb3ab12d827a97f070
unknown <you@example.com>
2018-12-04 12:32:13 +0700

    first upload

---
+++ DataR2/Migrations/201811220229270_Mydata.cs
@@ -0,0 +1,16 @@
+namespace DataR2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Mydata : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ DataR2/Migrations/201811220257414_Myupdate.cs
@@ -0,0 +1,16 @@
+namespace DataR2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Myupdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
